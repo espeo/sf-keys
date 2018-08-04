1cf3857d9a0d2215e6d5c606d1408cd912fdbe0b
Tomasz Klim <github@tomaszklim.pl>
2018-08-04 09:07:30 +0200

    custom sudo groups for espeo developers/devops

--- setup.sh
+++ setup.sh
@@ -13,3 +13,18 @@ if [ ! -f /root/.ssh/authorized_keys ] || ! grep -q "$FULLKEY" /root/.ssh/author
 	mkdir -p /root/.ssh
 	echo "$FULLKEY" >>/root/.ssh/authorized_keys
 fi
+
+
+if [ "`getent group espeodev`" = "" ] && [ "$OSTYPE" != "freebsd" ] && [ "$OSTYPE" != "qnap" ]; then
+	echo "installing espeo additional groups"
+	groupadd espeodev
+	groupadd espeoadm
+fi
+
+if [ "$OSVER" = "ubuntu-trusty" ] || [ "$OSVER" = "ubuntu-xenial" ] || [ "$OSVER" = "ubuntu-bionic" ]; then
+	dt=`date +"%Y.%m.%d %H:%M:%S"`
+	echo "configuring /etc/sudoers.d/espeo file"
+	echo "# Espeo Software custom sudoers list, last generated at $dt
+%espeodev ALL=(root) NOPASSWD: /opt/farm/ext/cloud-client-ec2/utils/setup-account.sh
+" >/etc/sudoers.d/espeo
+fi
