4e6875c619e63a7079d90c608711925a0cd0a455
Tomasz Klim <github@tomaszklim.pl>
2018-12-15 23:18:05 +0100

    use sf-passwd-utils extension to manage root management key

--- setup.sh
+++ setup.sh
@@ -1,22 +1,15 @@
 #!/bin/sh
 . /opt/farm/scripts/init
 
-
-echo "preparing ssh key"
-FULLKEY=`/opt/farm/ext/keys/get-ssh-management-key-content.sh $HOST`
-
-if [ ! -f /root/.ssh/authorized_keys ] || ! grep -q "$FULLKEY" /root/.ssh/authorized_keys; then
-	echo "setting up root ssh key"
-	mkdir -p /root/.ssh
-	echo "$FULLKEY" >>/root/.ssh/authorized_keys
-fi
-
-
 /opt/farm/scripts/setup/extension.sh sf-passwd-utils
+
+key=`/opt/farm/ext/keys/get-ssh-management-key-content.sh $HOST`
+/opt/farm/ext/passwd-utils/add-key.sh root inline "$key"
+
+
 echo "checking custom system groups"
 /opt/farm/ext/passwd-utils/create-group.sh espeodev
 /opt/farm/ext/passwd-utils/create-group.sh espeoadm
-
 
 /opt/farm/scripts/setup/extension.sh sf-mc-black
 /opt/farm/scripts/setup/extension.sh sf-db-tools
