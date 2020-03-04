cf215d80bd3c6971412fdf7d0cfedf2cc98c47ee
Tomasz Klim <github@tomaszklim.pl>
2020-03-04 16:08:30 +0100

    IT-78 support for multiple ssh management keys

---
+++ setup-custom-sudoers.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+. /opt/farm/scripts/init
+
+if [ -f /etc/sudoers.d/espeo ] && [ "`which lsattr 2>/dev/null`" != "" ] && [ "`lsattr -l /etc/sudoers.d/espeo |grep Immutable`" != "" ]; then
+	echo "skipping sudoers setup, file is already configured and immutable"
+	exit 0
+fi
+
+if  [ "$OSTYPE" = "amazon" ] || \
+	[ "$OSVER" = "ubuntu-trusty" ] || \
+	[ "$OSVER" = "ubuntu-xenial" ] || \
+	[ "$OSVER" = "ubuntu-bionic" ] || \
+	[ "$OSVER" = "debian-buster" ] || \
+	[ "$OSVER" = "debian-stretch" ]; then
+	dt=`date +"%Y.%m.%d %H:%M:%S"`
+	echo "configuring /etc/sudoers.d/espeo file"
+	echo "# Espeo Software custom sudoers list, last generated at $dt
+%espeoadm ALL=(root) NOPASSWD: ALL
+" >/etc/sudoers.d/espeo
+fi
--- setup.sh
+++ setup.sh
@@ -1,51 +1,14 @@
 #!/bin/sh
 . /opt/farm/scripts/init
 
-add_root_key() {
-	key=`/opt/farm/ext/keys/get-ssh-management-key-content.sh $1`
-	/opt/farm/ext/passwd-utils/add-key.sh root inline "$key"
-}
-
-
-add_root_key $HOST
-
-# actual server hostname can differ from configured one
-current=`hostname`
-if [ "$HOST" != "$current" ]; then
-	add_root_key $current
-fi
-
-
-# TODO: detect also Microsoft Azure and Google Cloud (and only these)
-
-# detect Amazon EC2/ECS and install also keys for one of these
-if [ -f /etc/image-id ] && grep -q ami-ecs /etc/image-id; then
-	add_root_key ecs-null
-elif [ -d /sys/class/dmi/id ] && grep -qi amazon /sys/class/dmi/id/* 2>/dev/null; then
-	add_root_key .amazonaws.com
-fi
-
-
 echo "checking custom system groups"
 /opt/farm/ext/passwd-utils/create-group.sh espeodev
 /opt/farm/ext/passwd-utils/create-group.sh espeoadm
 
+/opt/farm/ext/keys/setup-root-keys.sh
+/opt/farm/ext/keys/setup-custom-sudoers.sh
 
 /opt/farm/scripts/setup/extension.sh sf-mc-black
 /opt/farm/scripts/setup/extension.sh sf-db-tools
 /opt/farm/scripts/setup/extension.sh sf-app-deploy
 /opt/farm/scripts/setup/extension.sh sf-circle
-
-
-if  [ "$OSTYPE" = "amazon" ] || \
-	[ "$OSVER" = "ubuntu-trusty" ] || \
-	[ "$OSVER" = "ubuntu-xenial" ] || \
-	[ "$OSVER" = "ubuntu-bionic" ] || \
-	[ "$OSVER" = "debian-buster" ] || \
-	[ "$OSVER" = "debian-stretch" ]; then
-	dt=`date +"%Y.%m.%d %H:%M:%S"`
-	echo "configuring /etc/sudoers.d/espeo file"
-	echo "# Espeo Software custom sudoers list, last generated at $dt
-%espeoadm ALL=(root) NOPASSWD: ALL
-" >/etc/sudoers.d/espeo
-fi
